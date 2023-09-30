7109d5d75bc2ed0bce7304191be69d8854cf3fa6
sothul <thornsothul@gmail.com>
2023-09-30 21:57:03 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# React Nike
+
+Build and Deploy a Nike Website with tailwindcss and react
+
+[`Live View`]()
+
+## Screenshot
+
+![](/src/screenshot/home.png)
+![](/src/screenshot/product.png)
+![](/src/screenshot/about-us.png)
+![](/src/screenshot/special-offer.png)
+![](/src/screenshot/customer-reviews.png)
+![](/src/screenshot/contact-us.png)
+
+Thanks to [`Javascript Mastery`](https://nextjs-promptopia-l141zr44z-sothuls-projects.vercel.app/)
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  SuperQuality,
+  SpecialOffer,
+  Subscribe,
+  CustomerReviews,
+  Footer,
+};
